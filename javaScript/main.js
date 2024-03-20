877aedbc15bb7707928b0c87faf9d60991b75c56
Yoon-Jae-Min <jmyoon1994@naver.com>
2024-03-20 23:05:34 +0900

    20240320-home

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,7 +1,21 @@
+let menu = document.querySelector('#menu');
 let menuButton = document.querySelector('#menu-button');
 let menuContentIcons = document.querySelectorAll('.menu-content-icon');
 let menuContentText = document.querySelector('.menu-content-text');
 let menuContentTexts = document.querySelectorAll('.menu-content-text');
+
+
+menu.addEventListener("mouseover", () => {
+    menu.classList.add("active");
+    menuContentIcons.forEach(icon => icon.classList.add("active"));
+    menuContentTexts.forEach(text => text.classList.add("active"));
+});
+
+menu.addEventListener("mouseout", () => {
+    menu.classList.remove("active");
+    menuContentIcons.forEach(icon => icon.classList.remove("active"));
+    menuContentTexts.forEach(text => text.classList.remove("active"));
+});
 
 menuButton.addEventListener("click", () => {
 
@@ -17,6 +31,3 @@ menuButton.addEventListener("click", () => {
     }
 });
 
-// menu.addEventListener("mouseout", () => {
-    
-// });
