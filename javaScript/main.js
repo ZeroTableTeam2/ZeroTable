62380ad869c2a0c87d96160eeeb503dc23430c59
Yoon-jae-min <jmyoon1994@naver.com>
2024-03-21 18:03:01 +0900

    20240321

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,4 +1,5 @@
 let menu = document.querySelector('#menu');
+let article = document.querySelector('article');
 let menuButton = document.querySelector('#menu-button');
 let menuContentIcons = document.querySelectorAll('.menu-content-icon');
 let menuContentText = document.querySelector('.menu-content-text');
@@ -7,12 +8,14 @@ let menuContentTexts = document.querySelectorAll('.menu-content-text');
 
 menu.addEventListener("mouseover", () => {
     menu.classList.add("active");
+    article.classList.add("active");
     menuContentIcons.forEach(icon => icon.classList.add("active"));
     menuContentTexts.forEach(text => text.classList.add("active"));
 });
 
 menu.addEventListener("mouseout", () => {
     menu.classList.remove("active");
+    article.classList.remove("active");
     menuContentIcons.forEach(icon => icon.classList.remove("active"));
     menuContentTexts.forEach(text => text.classList.remove("active"));
 });
@@ -21,11 +24,13 @@ menuButton.addEventListener("click", () => {
 
     if(menuContentText.classList.contains('active')){
         menu.classList.remove("active");
+        article.classList.remove("active");
         menuContentIcons.forEach(icon => icon.classList.remove("active"));
         menuContentTexts.forEach(text => text.classList.remove("active"));
     }
     else{
         menu.classList.add("active");
+        article.classList.add("active");
         menuContentIcons.forEach(icon => icon.classList.add("active"));
         menuContentTexts.forEach(text => text.classList.add("active"));
     }
