6eb30fccd8ab93761bb6becad1ba5e5b4efe2bc0
Yoon-Jae-Min <jmyoon1994@naver.com>
2024-03-24 01:37:01 +0900

    20240324-home

--- javaScript/login.js
+++ javaScript/login.js
@@ -1,10 +1,10 @@
-function changeContainer(id){
-    let containers = document.querySelectorAll('container');
+function changeContainer(classId){
+    let containers = document.querySelectorAll('.container');
     containers.forEach(container => container.classList.remove("containerActive"));
     containers.forEach(container => container.classList.remove("containerNone"));
 
     for(let i = 0; i < containers.length; i++){
-        if(containers[i] == document.getElementById(id)){
+        if(containers[i] == document.querySelector(`.${classId}`)){
             containers[i].classList.add("containerActive");
         }
         else{
